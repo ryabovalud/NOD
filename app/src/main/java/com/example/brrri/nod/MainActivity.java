aecf7b8f46028b38c8fb830ae51470d50c053e1c
Luda <brrrinar@gmail.com>
2018-10-11 13:46:37 +1000

    with two windows(2 - for answer NOD)

--- app/src/main/java/com/example/brrri/nod/MainActivity.java
+++ app/src/main/java/com/example/brrri/nod/MainActivity.java
@@ -6,10 +6,13 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.util.Log;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "myLogs";
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +20,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onButtonClick (View w) {
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText el1 = (EditText)findViewById(R.id.Num1);
         EditText el2 = (EditText)findViewById(R.id.Num2);
-        TextView resText = (TextView)findViewById(R.id.Result);
 
         int num1 = Integer.parseInt(el1.getText().toString()),
                 num2 = Integer.parseInt(el2.getText().toString());
@@ -35,7 +39,8 @@ public class MainActivity extends AppCompatActivity {
         }
         resNod = a;
         Log.d(TAG, "НОД = "+resNod);
-
-        resText.setText(Integer.toString(resNod));
+        intent.putExtra(EXTRA_MESSAGE, "NOD = "+resNod);
+        startActivity(intent);
     }
-}
+    }
+
